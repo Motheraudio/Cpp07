2f26b27054e32e3e075cb898a85674a063978490
Alvaro Jose Campos Mendez <alvaro.camposmndz@gmail.com>
2026-03-02 18:20:30 +0100

    fixed leak/crash on non matching sizes ex02

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -2,47 +2,56 @@
 
 #include <cstdlib>
 #include <exception>
-template <typename T>
-class Array {
+template <typename T> class Array
+{
 public:
-  Array(){this->arr = NULL; this->sz = 0;};
-  Array(unsigned int n){ 
-	  this->sz = n;
-	  if (n == 0)
-		  this->arr = NULL;
-	  else
-		this->arr = new T[n];
-	  for (unsigned int i = 0; i < n; i++)
-		  this->arr[i] = 0;
+  Array()
+  {
+    this->arr = NULL;
+    this->sz = 0;
   };
-  Array(const Array& obj){
-	  if (obj.arr != NULL)
-		this->arr = new T[obj.sz];
-	  else
-		  this->arr = NULL;
-	  *this = obj;
+  Array(unsigned int n)
+  {
+    this->sz = n;
+    if (n == 0)
+      this->arr = NULL;
+    else
+      this->arr = new T[n];
+    for (unsigned int i = 0; i < n; i++)
+      this->arr[i] = 0;
+  };
+  Array(const Array &obj) : sz(obj.sz)
+  {
+    if (obj.arr != NULL)
+      this->arr = new T[obj.sz];
+    else
+      this->arr = NULL;
+    *this = obj;
+  }
+  ~Array() { delete[] this->arr; };
+  Array &operator=(const Array &obj)
+  {
+    if (this == &obj)
+      return (*this);
+    if (this->sz != obj.sz)
+    {
+      delete[] this->arr;
+      this->arr = new T[obj.sz];
+    }
+    this->sz = obj.sz;
+    for (unsigned int i = 0; i < this->sz; i++)
+      this->arr[i] = obj.arr[i];
+    return (*this);
+  };
+  T &operator[](const size_t index)
+  {
+    if (index >= this->sz)
+      throw(std::exception());
+    return (this->arr[index]);
+  }
+  unsigned int &size() { return (this->sz); }
 
-  }
-  ~Array(){delete[] this->arr;};
-  Array& operator=(const Array& obj){
-	  this->sz = obj.sz;
-	  if (this->sz == 0)
-		  return (*this);
-	  for (unsigned int i = 0; i < this->sz; i++)
-		  this->arr[i] = obj.arr[i];
-	  return (*this);
-  };
-  T& operator[](const size_t index){
-	  if (index >= this->sz)
-		  throw (std::exception());
-	  return (this->arr[index]);
-  }
-  unsigned int& size() const{
-	return (this->size);
-  }
 private:
   T *arr;
   unsigned int sz;
 };
-
-
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,57 +1,61 @@
-#include <iostream>
-#include <cstdlib>
 #include "Array.hpp"
+#include <cstdlib>
+#include <iostream>
 
-#define MAX_VAL 750
-int main(int, char**)
+#define MAX_VAL 5
+int main(int, char **)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-		// std::cout << numbers[i] << std::endl;
-        mirror[i] = value;
-		// std::cout << mirror[i] << std::endl;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+  Array<int> numbers(MAX_VAL);
+  int *mirror = new int[MAX_VAL];
+  srand(time(NULL));
+  for (int i = 0; i < MAX_VAL; i++)
+  {
+    const int value = rand();
+    numbers[i] = value;
+    // std::cout << numbers[i] << std::endl;
+    mirror[i] = value;
+    // std::cout << mirror[i] << std::endl;
+  }
+  // SCOPE
+  {
+    Array<int> tmp = numbers;
+    Array<int> test(tmp);
+  }
 
-    for (int i = 0; i < MAX_VAL; i++)
+  for (int i = 0; i < MAX_VAL; i++)
+  {
+    if (mirror[i] != numbers[i])
     {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
+      std::cerr << "didn't save the same value!!" << std::endl;
+      return 1;
     }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+  }
+  try
+  {
+    numbers[-2] = 0;
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+  try
+  {
+    numbers[MAX_VAL] = 0;
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << e.what() << '\n';
+  }
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-		// std::cout << numbers[i] << std::endl;
-    }
-    delete [] mirror;//
-    return 0;
+  for (int i = 0; i < MAX_VAL; i++)
+  {
+    numbers[i] = rand();
+    // std::cout << numbers[i] << std::endl;
+  }
+  delete[] mirror;
+  Array<int> yo(2);
+  std::cout << yo.size() << std::endl;
+  yo = numbers;
+  std::cout << yo.size() << std::endl;
+  return 0;
 }
